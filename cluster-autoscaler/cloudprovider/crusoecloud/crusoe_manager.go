cea76364af8b9916b1780f02d24c71cbba1595f5
Ariel <asalomon@crusoeenergy.com>
2024-12-18 17:12:15 -0800

    Refactor Crusoe client operations into manager

---
+++ cluster-autoscaler/cloudprovider/crusoecloud/crusoe_manager.go
@@ -0,0 +1,135 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package crusoecloud
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/antihax/optional"
+	crusoeapi "github.com/crusoecloud/client-go/swagger/v1alpha5"
+)
+
+type crusoeManager struct {
+	// client talks to Crusoecloud API
+	client *crusoeapi.APIClient
+	// ProjectID is the project id containing the CMK cluster.
+	projectID string
+	// ClusterID is the CMK cluster id where the Autoscaler is running.
+	clusterID string
+}
+
+func NewManager(cfg *crusoeCloudConfig, userAgent string) *crusoeManager {
+	client := NewAPIClient(cfg.APIEndpoint, cfg.AccessKey, cfg.SecretKey, userAgent)
+	return &crusoeManager{
+		client:    client,
+		projectID: cfg.ProjectID,
+		clusterID: cfg.ClusterID,
+	}
+}
+
+func (mgr *crusoeManager) ListNodePools(ctx context.Context) ([]crusoeapi.KubernetesNodePool, error) {
+	resp, httpResp, err := mgr.client.KubernetesNodePoolsApi.ListNodePools(ctx, mgr.projectID,
+		&crusoeapi.KubernetesNodePoolsApiListNodePoolsOpts{ClusterId: optional.NewString(mgr.clusterID)})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to list pools for cluster %s: %w", mgr.clusterID, err)
+	}
+	if httpResp.StatusCode >= 400 {
+		return nil, fmt.Errorf("failed to list pools for cluster %s: http error %s",
+			mgr.clusterID, httpResp.Status)
+	}
+	return resp.Items, nil
+}
+
+func (mgr *crusoeManager) GetNodePool(ctx context.Context, poolID string) (*crusoeapi.KubernetesNodePool, error) {
+	resp, httpResp, err := mgr.client.KubernetesNodePoolsApi.GetNodePool(ctx, mgr.projectID, poolID)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get nodepool %s: %w", poolID, err)
+	}
+	if httpResp.StatusCode >= 400 {
+		return nil, fmt.Errorf("failed to get nodepool %s: http error %s", poolID, httpResp.Status)
+	}
+	return &resp, nil
+}
+
+func (mgr *crusoeManager) UpdateNodePool(ctx context.Context, poolID string, targetSize int64) (*crusoeapi.Operation, error) {
+	resp, httpResp, err := mgr.client.KubernetesNodePoolsApi.UpdateNodePool(ctx,
+		crusoeapi.KubernetesNodePoolPatchRequest{Count: targetSize},
+		mgr.projectID, poolID)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to update node pool %s for cluster %s: %w", poolID, mgr.clusterID, err)
+	}
+	if httpResp.StatusCode >= 400 {
+		return nil, fmt.Errorf("failed to update node pool %s for cluster %s: http error %s",
+			poolID, mgr.clusterID, httpResp.Status)
+	}
+	return resp.Operation, nil
+}
+
+func (mgr *crusoeManager) GetNodePoolOperation(ctx context.Context, opID string) (*crusoeapi.Operation, error) {
+	resp, httpResp, err := mgr.client.KubernetesNodePoolOperationsApi.GetKubernetesNodePoolsOperation(ctx, mgr.projectID, opID)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get nodepool operation %s: %w", opID, err)
+	}
+	if httpResp.StatusCode >= 400 {
+		return nil, fmt.Errorf("failed to get nodepool operation %s: http error %s", opID, httpResp.Status)
+	}
+	return &resp, nil
+}
+
+func (mgr *crusoeManager) ListVMInstances(ctx context.Context, instanceIds []string) ([]crusoeapi.InstanceV1Alpha5, error) {
+	resp, httpResp, err := mgr.client.VMsApi.ListInstances(ctx, mgr.projectID, &crusoeapi.VMsApiListInstancesOpts{
+		Ids: optional.NewString(strings.Join(instanceIds, ",")),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list instances for cluster %s: %w", mgr.clusterID, err)
+	}
+	if httpResp.StatusCode >= 400 {
+		return nil, fmt.Errorf("failed to list instances for cluster %s: http error %s",
+			mgr.clusterID, httpResp.Status)
+	}
+	return resp.Items, nil
+}
+
+func (mgr *crusoeManager) DeleteVMInstance(ctx context.Context, instanceId string) (*crusoeapi.Operation, error) {
+	resp, httpResp, err := mgr.client.VMsApi.DeleteInstance(ctx, mgr.projectID, instanceId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to delete instance %s for cluster %s: %w", instanceId, mgr.clusterID, err)
+	}
+	if httpResp.StatusCode >= 400 {
+		return nil, fmt.Errorf("failed to delete instance %s for cluster %s: http error %s",
+			instanceId, mgr.clusterID, httpResp.Status)
+	}
+	return resp.Operation, nil
+}
+
+func (mgr *crusoeManager) GetVMOperation(ctx context.Context, opID string) (*crusoeapi.Operation, error) {
+	resp, httpResp, err := mgr.client.VMOperationsApi.GetComputeVMsInstancesOperation(ctx, mgr.projectID, opID)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get VM operation %s: %w", opID, err)
+	}
+	if httpResp.StatusCode >= 400 {
+		return nil, fmt.Errorf("failed to get VM operation %s: http error %s", opID, httpResp.Status)
+	}
+	return &resp, nil
+}
